903e8eb15b6e2cd758a4c2570859d6815e5dd391
OzRAGEHarm <eabot864@gmail.com>
2025-02-02 16:54:22 -0600

    Version: 2.0.0

---
+++ src/models.js
@@ -0,0 +1,23 @@
+const models = {
+  deepseek: {
+      v3: 'deepseek-ai/DeepSeek-V3',
+      r1: 'deepseek-ai/DeepSeek-R1',
+      llm67b: 'deepseek-ai/deepseek-llm-67b-chat',
+  },
+  mistral: {
+      small24B: 'mistralai/Mistral-Small-24B-Instruct-2501',
+  },
+  databricks: {
+      instruct: 'databricks/dbrx-instruct',
+  },
+  qwen: {
+      preview: 'Qwen/QwQ-32B-Preview',
+  },
+  nous: {
+      hermes: 'NousResearch/Nous-Hermes-2-Mixtral-8x7B-DPO',
+  },
+};
+
+module.exports = {  
+  models  
+};
